e6f25b4b598c79428e6836f693fc7f5570e6e82e
gbelot2003 <gbelot2003@hotmail.com>
2019-02-14 00:25:22 -0600

    Probando funciones de singup y login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -12,17 +15,21 @@ class AuthController extends Controller
      */
     public function signup(Request $request)
     {
+        //dd($request);
         $request->validate([
             'name' => 'required|string',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|confirmed'
         ]);
+
         $user = new User([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password)
         ]);
+
         $user->save();
+
         return response()->json([
             'message' => 'Successfully created user!'
         ], 201);
@@ -30,29 +37,38 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $request->validate([
+        /*$request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
             'remember_me' => 'boolean'
-        ]);
+        ]);*/
+
         $credentials = request(['email', 'password']);
+
         if (!Auth::attempt($credentials))
             return response()->json([
                 'message' => 'Unauthorized'
             ], 401);
         $user = $request->user();
-        $tokenResult = $user->createToken('Personal Access Token');
+
+        $tokenResult = $user->createToken('Personal Access Token')->accessToken;
+        dd($tokenResult);
+
         $token = $tokenResult->token;
+
         if ($request->remember_me)
             $token->expires_at = Carbon::now()->addWeeks(1);
+
         $token->save();
+
         return response()->json([
             'access_token' => $tokenResult->accessToken,
             'token_type' => 'Bearer',
             'expires_at' => Carbon::parse(
                 $tokenResult->token->expires_at
             )->toDateTimeString()
-        ]);
+
+        ], 200);
     }
 
     /**
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,21 +2,55 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Laravel\Passport\Passport;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    use WithFaker, RefreshDatabase;
+
+
+    /** @test */
+    public function api_create_user()
+    {
+        $user = [
+            'name' => 'Gerard',
+            'email' => 'looh@out.com',
+            'password' => 'Luna0102',
+            'password_confirmation' => 'Luna0102'
+        ];
+
+        $this->json('POST', '/api/auth/signup', $user, ['Accept' => 'application/json'])
+            ->assertStatus(201);
     }
+
+    /** @test */
+    public function api_login_user()
+    {
+        $user = [
+            'name' => 'Gerard',
+            'email' => 'looh@out.com',
+            'password' => 'Luna0102',
+            'password_confirmation' => 'Luna0102'
+        ];
+
+        $luser = [
+            'email' => 'looh@out.com',
+            'password' => 'Luna0102',
+            'remember_me' => true
+        ];
+
+        $this->json('POST', '/api/auth/signup', $user, ['Accept' => 'application/json'])
+            ->assertStatus(201);
+
+        $this->artisan('passport:install');
+
+        $this->json('post', '/api/auth/login', $luser, ['Accept' => 'application/json'])
+            ->assertStatus(200);
+    }
+
 }
